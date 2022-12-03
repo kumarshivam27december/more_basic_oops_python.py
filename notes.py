3b0f421d8a3e0ba56b9fb3c02adfe3b6c87bb95d
Shivam Kumar <kumarshivam16062004@gmail.com>
2022-12-04 00:13:27 +0530

    version 1.0

---
+++ notes.py
@@ -0,0 +1,49 @@
+'''
+Python Object Oriented Programming
+In this tutorial, you'll learn about 
+Object-Oriented Programming (OOP) in Python and
+its fundamental concept with the help of examples.
+
+
+
+Object Oriented Programming
+Python is a multi-paradigm programming language. 
+It supports different programming approaches.
+
+One of the popular approaches to solve a programming 
+problem is by creating objects. 
+This is known as Object-Oriented Programming (OOP).
+
+An object has two characteristics:
+
+attributes
+behavior
+Let's take an example:
+
+A parrot is an object, as it has the following properties:
+
+name, age, color as attributes
+singing, dancing as behavior
+The concept of OOP in Python focuses on creating 
+reusable code. This concept is also known 
+as DRY (Don't Repeat Yourself).
+
+In Python,
+the concept of OOP follows some basic principles:
+
+Class
+A class is a blueprint for the object.
+
+We can think of class as a sketch of a parrot with labels. It contains all the details about the name, colors, size etc. Based on these descriptions, we can study about the parrot. Here, a parrot is an object.
+
+The example for class of parrot can be :
+
+class Parrot:
+    pass
+Here, we use the class keyword to define an empty class Parrot. From class, we construct instances. An instance is a specific object created from a particular class.
+
+Object
+An object (instance) is an instantiation of a class. When class is defined, only the description for the object is defined. Therefore, no memory or storage is allocated.
+
+
+'''
